f2ff28aabca7cfe7b56929df4c40eac40e3f000f
piendi <dierk.bent.piening@gmail.com>
2015-07-25 18:11:09 +0200

    asasas

--- commit.sh
+++ commit.sh
@@ -9,7 +9,7 @@ if NAME=$(zenity --entry --text "Bitte geben sie den Gwünschten Commit-Title ei
 			zenity --text-info --text "Alle Dateien werden hinzugefügt";
 			if git commit -m "$NAME"; then
 				{
-					zenity --text-info --text "Commit mit der Nachricht $NAME";
+					zenity ---info --text "Commit mit der Nachricht $NAME";
 					git push | zenity --info --width 530; 
 				}
 			fi
