5ef3a8b55ca45be467ebe6f879769f66ab964d15
piendi <dierk.bent.piening@gmail.com>
2015-07-25 18:13:53 +0200

    qwdqwdqwd

--- commit.sh
+++ commit.sh
@@ -9,8 +9,8 @@ if NAME=$(zenity --entry --text "Bitte geben sie den Gwünschten Commit-Title ei
 			zenity --info --text "Alle Dateien werden hinzugefügt";
 			if git commit -m "$NAME"; then
 				{
-					zenity ---info --text "Commit mit der Nachricht $NAME";
-					git push | zenity --text-info --width 530; 
+					zenity ---notification --text "Commit mit der Nachricht $NAME";
+					git push | zenity --info --width 530; 
 				}
 			fi
 		}
